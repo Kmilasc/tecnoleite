ef284109a73712dd1f6d1ca7e790498dd992c870
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-07-26 22:16:54 -0300

    refactor(SpeakerPresentation): fixing header color

--- src/pages/Home/components/TeamSection/components/SpeakerPresentation.tsx
+++ src/pages/Home/components/TeamSection/components/SpeakerPresentation.tsx
@@ -9,10 +9,13 @@ export function SpeakerPresentation({ imageUrl, name, title, about }: Props) {
     return (
         <div className="flex flex-col items-center gap-y-4">
             <div className="flex flex-col items-center">
-                <span className="text-md xs:text-xl sm:text-2xl font-conforter text-black font-bold border-spacing-1 border-b-4 " style={{borderColor: '#308E96' }}>
+                <span
+                    className="text-md xs:text-xl sm:text-2xl font-conforter text-black font-bold border-spacing-1 border-b-4 "
+                    style={{ borderColor: '#308E96' }}
+                >
                     {name}
                 </span>
-                <p className="text-sm xs:text-md sm:text-xl text-gray-400">{title}</p>
+                <p className="text-sm xs:text-md sm:text-xl text-gray-600">{title}</p>
             </div>
             <img src={imageUrl} alt="palestrante" className="w-60" />
             <p className="text-center">{about}</p>
