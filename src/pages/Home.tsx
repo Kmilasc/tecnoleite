fc64c1054f7027d784f2cd6394ad7004c29f23a7
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-07-26 22:00:11 -0300

    refactor(SponsorsSection): creating component and adding responsiveness

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,5 @@
-import resolveConfig from 'tailwindcss/resolveConfig';
-import tailwindConfig from '../../tailwind.config.js';
-
 import bgHome from '../assets/bgHome.png';
-import logoIfood from '../assets/logoIfood.png'
-import logoAmazon from '../assets/logoAmazon.png'
-import logoNubank from '../assets/logoNubank.jpg'
+
 import sobre from '../assets/sobre.png';
 import 'react-vertical-timeline-component/style.min.css';
 import { EventBanner } from './Home/components/EventBanner.js';
@@ -13,8 +8,7 @@ import { Menu } from './Home/components/Menu.js';
 import { Footer } from './Home/components/Footer.js';
 import { TimelineSection } from './Home/components/TimelineSection.js';
 import { TeamSection } from './Home/components/TeamSection/index.js';
-
-const fullConfig = resolveConfig(tailwindConfig);
+import { SponsorsSection } from './Home/components/SponsorsSection/index.js';
 
 const events = [
     { id: 1, date: 'Day 1', text: 'Text bla bla bla bla' },
@@ -54,18 +48,15 @@ const speakers = [
 
 export default function Home() {
     return (
-        
         <div className="w-full">
             <div className="min-h-full flex flex-col">
                 <Menu />
                 <header>
-                    
                     <EventBanner
                         imageUrl={bgHome}
                         title="3º Tecnoleite"
                         subtitle={`Produção de leite com tecnologia e sustentabilidade ${'\n'} 1 e 2 de Setembro`}
                     />
-                    
                 </header>
                 <main className="w-full flex flex-col px-16 gap-y-12">
                     <AboutSection
@@ -80,50 +71,12 @@ export default function Home() {
                         texto texto texto texto texto"
                         imageUrl={sobre}
                     />
-                    <div className="grid grid-cols-1 xl:grid-cols-2 gap-10">
-                        <div className="flex-1 w-full text-2xl xs:text-4xl sm:text-6xl font-conforter text-black font-bold border-spacing-1 text-center xl:text-start xl:self-center">
-                            <span className="border-b-4" style={{borderColor: '#308E96' }}>Patrocinadores</span>
-                            <div className='relative text-center'>
-                                <a href="https://www.ifood.com.br/" target="_blank" style={{ display: "inline-block" }}>
-                                    <img className='mt-14 w-40 h-32' src={logoIfood} alt="Logo iFood" />
-                                </a>
-                                <p className="text-xl font-normal mt-8 flex-1">
-                                    Texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                    texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                    texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                </p>
-                            </div>
-
-                            <div className='relative text-center'>
-                                <a href="https://www.Amazon.com.br/" target="_blank" style={{ display: "inline-block" }}>
-                                    <img className='mt-14 w-40 h-32' src={logoAmazon} alt="Logo Amazon" />
-                                </a>
-                            </div>
-                            <p className="text-xl font-normal mt-8 flex-1 relative text-center">
-                                Texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            </p>
-                            <div className='relative text-center'>
-                                <a href="https://www.Nubank.com.br/" target="_blank" style={{ display: "inline-block" }}>
-                                    <img className='mt-14 w-40 h-32' src={logoNubank} alt="Logo Nubank" />
-                                </a>
-                            </div>
-                            <p className="text-xl font-normal mt-8 flex-1 relative text-center">
-                                Texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                                texto texto texto texto texto texto texto texto texto texto texto texto texto texto texto
-                            </p>
-                        </div> 
-                    </div>
-
-                    <TimelineSection events={events} lineColor={"#308E96"} />
+                    <SponsorsSection />
+                    <TimelineSection events={events} lineColor="#308E96" />
                     <TeamSection speakers={speakers} />
                 </main>
                 <Footer />
-                
             </div>
-            
         </div>
     );
 }
---
+++ src/pages/Home/components/SponsorsSection/components/SponsorPresentation.tsx
@@ -0,0 +1,17 @@
+interface SponsorProps {
+    url: string;
+    logo: string;
+    alt: string;
+    text: string;
+}
+
+export function SponsorPresentation({ url, logo, alt, text }: SponsorProps) {
+    return (
+        <div className="flex flex-col items-center gap-y-12">
+            <a href={url} target="_blank" rel="noreferrer">
+                <img className="w-40 h-32" src={logo} alt={alt} />
+            </a>
+            <p className="text-xl font-normal flex-1 text-center">{text}</p>
+        </div>
+    );
+}
