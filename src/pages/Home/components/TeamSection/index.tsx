43930b9f21305faa0bfc3869c4a2567eb9edd85f
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-07-26 21:55:26 -0300

    refactor(TeamSection): fixing header text position

--- src/pages/Home/components/TeamSection/index.tsx
+++ src/pages/Home/components/TeamSection/index.tsx
@@ -10,10 +10,15 @@ interface Props {
 
 export function TeamSection({ speakers }: Props) {
     return (
-        <div id='equipe' className="flex flex-col gap-y-10">
-            <h4 className="text-2xl xs:text-4xl sm:text-6xl font-conforter text-black font-bold border-spacing-1 border-b-4 " style={{borderColor: '#308E96' }}>
-                EQUIPE
-            </h4>
+        <div id="equipe" className="flex flex-col gap-y-10">
+            <div className="flex flex-col items-center">
+                <h4
+                    className="text-2xl xs:text-4xl sm:text-6xl font-conforter text-black font-bold border-spacing-1 border-b-4 "
+                    style={{ borderColor: '#308E96' }}
+                >
+                    EQUIPE
+                </h4>
+            </div>
             <div className="flex flex-col sm:grid sm:auto-cols-max sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {speakers.map(({ id, name, title, imageUrl, about }) => (
                     <SpeakerPresentation key={id} name={name} title={title} imageUrl={imageUrl} about={about} />
