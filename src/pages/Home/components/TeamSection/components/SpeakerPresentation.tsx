fb4377266e70bba48f8f26bb6eb3b2f57dbd0f09
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-08-10 22:14:11 -0300

    refactor(SpeakerPresentation): updating style

--- src/pages/Home/components/TeamSection/components/SpeakerPresentation.tsx
+++ src/pages/Home/components/TeamSection/components/SpeakerPresentation.tsx
@@ -1,3 +1,6 @@
+import { Modal } from 'flowbite-react';
+import { useState } from 'react';
+
 export interface Props {
     name: string;
     title: string;
@@ -6,21 +9,40 @@ export interface Props {
 }
 
 export function SpeakerPresentation({ imageUrl, name, title, about }: Props) {
+    const [openModal, setOpenModal] = useState<string | undefined>();
+
     return (
-        <div className="flex flex-col items-center gap-y-4 p-8 border border-main-600 hover:bg-main-200 rounded-xl bg-main-100">
-            <div className="flex flex-col items-center">
-                <span
-                    className="text-md xs:text-xl sm:text-2xl font-conforter text-black font-bold border-spacing-1 border-b-4 "
-                    style={{ borderColor: '#308E96' }}
-                >
-                    {name}
-                </span>
-                <div className="flex md:min-h-[75px] items-center">
-                    <p className="text-sm xs:text-md sm:text-xl text-center text-gray-600">{title}</p>
+        <>
+            <div aria-hidden onClick={() => setOpenModal('default')} className="flex flex-col items-center gap-y-4 p-8">
+                <img src={imageUrl} alt="palestrante" className="w-60 rounded-full" />
+                <div className="flex flex-col items-center">
+                    <span
+                        className="text-md xs:text-xl sm:text-2xl font-conforter text-black font-bold border-spacing-1 border-b-4 "
+                        style={{ borderColor: '#308E96' }}
+                    >
+                        {name}
+                    </span>
+                    <div className="flex md:min-h-[75px] items-center">
+                        <p className="text-sm xs:text-md sm:text-xl text-center text-gray-600">{title}</p>
+                    </div>
                 </div>
             </div>
-            <img src={imageUrl} alt="palestrante" className="w-60 rounded-full" />
-            <p className="text-center">{about}</p>
-        </div>
+            <Modal show={openModal === 'default'} onClose={() => setOpenModal(undefined)}>
+                <Modal.Header>{name}</Modal.Header>
+                <Modal.Body className="w-full">
+                    <div className="flex flex-col sm:flex-row gap-4">
+                        <img src={imageUrl} alt="palestrante" className="h-40 w-40 flex-1 rounded-full self-center" />
+                        <div className="flex flex-col gap-y-4">
+                            <div className="flex flex-col items-center">
+                                <div className="flex md:min-h-[75px] items-center">
+                                    <p className="text-sm xs:text-md sm:text-lg text-center text-gray-600">{title}</p>
+                                </div>
+                            </div>
+                            <p className="text-center">{about}</p>
+                        </div>
+                    </div>
+                </Modal.Body>
+            </Modal>
+        </>
     );
 }
