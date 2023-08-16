10e26744df818b082e31f4792f39a4aaf72ff4e1
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2023-08-16 14:46:21 -0300

    refactor(images): updating images

--- src/pages/Home/components/AboutSection.tsx
+++ src/pages/Home/components/AboutSection.tsx
@@ -8,11 +8,13 @@ export function AboutSection({ title, text, imageUrl }: Props) {
     return (
         <div className="grid grid-cols-1 xl:grid-cols-2 gap-10" id="about-section">
             <div className="flex-1 w-full text-2xl xs:text-4xl sm:text-6xl font-conforter text-black font-bold border-spacing-1 text-center xl:text-start xl:self-center">
-                <span className="border-b-4 " style={{borderColor: '#308E96' }}>{title}</span>
+                <span className="border-b-4 " style={{ borderColor: '#308E96' }}>
+                    {title}
+                </span>
                 <p className="text-xl font-normal mt-8 flex-1">{text}</p>
             </div>
             <img
-                className="flex-1 float-right w-96 lg:w-[600px] xl:w-full drop-shadow-lg place-self-center"
+                className="flex-1 float-right w-96 lg:w-[600px] xl:w-full max-w-[330px] drop-shadow-lg place-self-center"
                 src={imageUrl}
                 alt="ilustration"
             />
